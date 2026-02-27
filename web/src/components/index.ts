c5afd95ef03ab7228b4ae86fcea7b761b7dcebea
sanmipaul <sanmipaul1@gmail.com>
2026-02-27 23:36:09 +0100

    feat(components): create barrel index exporting StxAmount, StxAmountInput, and all existing components

---
+++ web/src/components/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Component barrel.
+ */
+export { default as StxAmount } from './StxAmount';
+export type { StxAmountProps, StxAmountHighlight } from './StxAmount';
+export { default as StxAmountInput } from './StxAmountInput';
+export type { StxAmountInputProps } from './StxAmountInput';
+export { default as CreateVaultModal } from './CreateVaultModal';
+export { default as VaultDetail } from './VaultDetail';
+export { default as PenaltyWarningModal } from './PenaltyWarningModal';
