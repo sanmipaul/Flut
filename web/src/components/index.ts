040f24a24fe0b879fd7e0ca208ecc5dbf0e52bd5
sanmipaul <sanmipaul1@gmail.com>
2026-02-27 21:46:42 +0100

    docs(components): add JSDoc header to component barrel and clean up re-exports

--- web/src/components/index.ts
+++ web/src/components/index.ts
@@ -1,3 +1,10 @@
+/**
+ * Component barrel.
+ * Import any UI component from this single entry-point.
+ *
+ * @example
+ *   import { CopyButton, VaultDetail } from './components';
+ */
 export { default as CopyButton } from './CopyButton';
 export type { CopyButtonProps, CopyButtonSize } from './CopyButton';
 export { default as CreateVaultModal } from './CreateVaultModal';
