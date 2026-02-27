3cc36708d10ddd71c0f037a3e267c99c8a344f75
sanmipaul <sanmipaul1@gmail.com>
2026-02-27 21:56:02 +0100

    feat(types): create barrel index re-exporting all VaultEvent types and constants

---
+++ web/src/types/index.ts
@@ -0,0 +1,13 @@
+export type {
+  VaultEvent,
+  VaultEventKind,
+  VaultEventSeverity,
+  VaultEventMeta,
+  VaultCreatedMeta,
+  BeneficiarySetMeta,
+  WithdrawalMeta,
+  EmergencyWithdrawalMeta,
+  LockExtendedMeta,
+} from './VaultEvent';
+
+export { EVENT_SEVERITY, EVENT_ICON, EVENT_LABEL } from './VaultEvent';
