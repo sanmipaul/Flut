e531a9bee28c43521a12ec26d42e85451f22b486
sanmipaul <sanmipaul1@gmail.com>
2026-02-27 23:22:41 +0100

    docs(context): add JSDoc header to context barrel describing public API surface

--- web/src/context/index.ts
+++ web/src/context/index.ts
@@ -1,1 +1,8 @@
+/**
+ * Context barrel.
+ * Import any React context provider or consumer hook from this entry-point.
+ *
+ * @example
+ *   import { ToastProvider, useToastContext } from './context';
+ */
 export { ToastProvider, useToastContext } from './ToastContext';
