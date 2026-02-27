181b538afe3ba5bd98030c05dc3fac7c0a7289b6
sanmipaul <sanmipaul1@gmail.com>
2026-02-27 23:18:28 +0100

    feat(components): create barrel index exporting ToastItem, ToastContainer, and all existing components

---
+++ web/src/components/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Component barrel.
+ * Import any UI component from this single entry-point.
+ */
+export { default as CreateVaultModal } from './CreateVaultModal';
+export { default as VaultDetail } from './VaultDetail';
+export { default as PenaltyWarningModal } from './PenaltyWarningModal';
+export { default as ToastItem } from './ToastItem';
+export type { ToastItemProps } from './ToastItem';
+export { default as ToastContainer } from './ToastContainer';
+export type { ToastContainerProps } from './ToastContainer';
