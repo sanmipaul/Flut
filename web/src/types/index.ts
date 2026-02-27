72c480e7bd44d33fddbbba25b3cd7c8d2031645b
sanmipaul <sanmipaul1@gmail.com>
2026-02-27 23:11:05 +0100

    feat(types): create barrel index re-exporting all Toast types and constants

---
+++ web/src/types/index.ts
@@ -0,0 +1,14 @@
+export type {
+  Toast,
+  ToastVariant,
+  ToastPosition,
+  ToastOptions,
+} from './Toast';
+
+export {
+  TOAST_DURATION_DEFAULT,
+  TOAST_DURATION_ERROR,
+  TOAST_DURATION_PERSISTENT,
+  TOAST_ICON,
+  TOAST_ROLE,
+} from './Toast';
