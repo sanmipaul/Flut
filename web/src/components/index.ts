76465cb2bee627c4c1fce561d24168441b15559c
sanmipaul <sanmipaul1@gmail.com>
2026-02-27 21:02:53 +0100

    feat(components): create barrel index exporting CopyButton and other components

---
+++ web/src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as CopyButton } from './CopyButton';
+export type { CopyButtonProps, CopyButtonSize } from './CopyButton';
+export { default as CreateVaultModal } from './CreateVaultModal';
+export { default as VaultDetail } from './VaultDetail';
+export { default as PenaltyWarningModal } from './PenaltyWarningModal';
