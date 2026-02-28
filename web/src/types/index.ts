25945242c17d86af4bd4d90efcd30fc24f853cd1
sanmipaul <sanmipaul1@gmail.com>
2026-02-28 06:38:14 +0100

    feat(types): add barrel index.ts exporting VaultSearch and VaultSearchResult types

---
+++ web/src/types/index.ts
@@ -0,0 +1,10 @@
+export type {
+  VaultStatusFilter,
+  VaultSortField,
+  VaultSortDirection,
+  VaultSearchState,
+  VaultSnapshot,
+} from './VaultSearch';
+export { defaultVaultSearchState } from './VaultSearch';
+
+export type { VaultSearchResult } from './VaultSearchResult';
