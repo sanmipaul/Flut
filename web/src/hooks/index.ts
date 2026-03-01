73f1228664534f2e066137cfeaa297b5060b59c7
sanmipaul <sanmipaul1@gmail.com>
2026-03-01 11:46:45 +0100

    chore(exports): add useStackingYield hook to hooks barrel export

--- web/src/hooks/index.ts
+++ web/src/hooks/index.ts
@@ -1,1 +1,6 @@
 export { useCountdown } from './useCountdown';
+export { useStackingYield } from './useStackingYield';
+export type {
+  UseStackingYieldInput,
+  UseStackingYieldReturn,
+} from './useStackingYield';
