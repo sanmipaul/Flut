4672984a5d382e280c7835b6e59261872f9b40cf
sanmipaul <sanmipaul1@gmail.com>
2026-03-01 11:46:38 +0100

    chore(exports): add calculateStackingYield and formatYield helpers to utils barrel export

--- web/src/utils/index.ts
+++ web/src/utils/index.ts
@@ -1,1 +1,8 @@
 export { blocksToSeconds, secondsToUnits, padTwo, buildAriaLabel } from './countdownUtils';
+export { calculateStackingYield } from './calculateStackingYield';
+export {
+  formatBtcAmount,
+  formatYieldPct,
+  formatCycleCount,
+  formatStxShort,
+} from './formatYield';
