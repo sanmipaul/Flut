f4c5e5aa8924911e33c873a34a23487ec98f31bc
sanmipaul <sanmipaul1@gmail.com>
2026-03-01 11:46:29 +0100

    chore(exports): add StackingYield types and constants to types barrel export

--- web/src/types/index.ts
+++ web/src/types/index.ts
@@ -5,3 +5,16 @@ export type {
   UseCountdownInput,
 } from './Countdown';
 export { SECONDS_PER_BLOCK } from './Countdown';
+
+export type {
+  StackingYieldInput,
+  StackingYieldResult,
+  StackingCycleReward,
+} from './StackingYield';
+export {
+  BLOCKS_PER_CYCLE,
+  SATOSHIS_PER_BTC,
+  DEFAULT_APY_PCT,
+  MIN_APY_PCT,
+  MAX_APY_PCT,
+} from './StackingYield';
