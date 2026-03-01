285b25f4aa9fb76bd1926aa4a47738e9fa5c8efe
sanmipaul <sanmipaul1@gmail.com>
2026-03-01 11:46:53 +0100

    chore(exports): add StackingYieldCard to components barrel export

--- web/src/components/index.ts
+++ web/src/components/index.ts
@@ -1,2 +1,5 @@
 export { default as VaultCountdown } from './VaultCountdown';
 export type { VaultCountdownProps } from './VaultCountdown';
+
+export { default as StackingYieldCard } from './StackingYieldCard';
+export type { StackingYieldCardProps } from './StackingYieldCard';
