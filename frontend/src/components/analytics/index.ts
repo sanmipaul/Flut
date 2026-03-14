77e09d16640e9cd4303e49d7de6ee58183fce470
sanmipaul <sanmipaul1@gmail.com>
2026-03-15 00:22:26 +0100

    chore: add barrel export for analytics components

---
+++ frontend/src/components/analytics/index.ts
@@ -0,0 +1,5 @@
+export { VaultStatusDistributionBar } from './VaultStatusDistributionBar';
+export { VaultBreakdownTable } from './VaultBreakdownTable';
+export { UpcomingUnlocksList } from './UpcomingUnlocksList';
+export { AnalyticsExportButton } from './AnalyticsExportButton';
+export { AnalyticsMetricGrid } from './AnalyticsMetricGrid';
