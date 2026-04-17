9e2f56dd89f14ed4867b9a63ab95bfa8d67d9de7
sanmipaul <sanmipaul1@gmail.com>
2026-04-17 09:44:46 +0100

    refactor(analytics): add barrel export for analytics UI state components

---
+++ web/src/components/analytics/index.ts
@@ -0,0 +1,4 @@
+export { EmptyState } from '../EmptyState';
+export { EmptyChartPlaceholder } from '../EmptyChartPlaceholder';
+export { FilteredEmptyState } from '../FilteredEmptyState';
+export { LoadingState } from '../LoadingState';
