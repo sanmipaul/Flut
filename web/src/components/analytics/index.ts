fd9c628c72bd458347f8b5851c87cf1852ed3118
sanmipaul <sanmipaul1@gmail.com>
2026-04-17 09:45:52 +0100

    refactor(analytics): add NoVaultState and AnalyticsEmptyIcon to analytics barrel export

--- web/src/components/analytics/index.ts
+++ web/src/components/analytics/index.ts
@@ -2,3 +2,5 @@ export { EmptyState } from '../EmptyState';
 export { EmptyChartPlaceholder } from '../EmptyChartPlaceholder';
 export { FilteredEmptyState } from '../FilteredEmptyState';
 export { LoadingState } from '../LoadingState';
+export { NoVaultState } from '../NoVaultState';
+export { AnalyticsEmptyIcon } from '../AnalyticsEmptyIcon';
