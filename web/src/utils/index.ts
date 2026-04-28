a360d30bdb41733fef203c92de53938c48cdaeae
sanmipaul <sanmipaul1@gmail.com>
2026-04-28 12:18:14 +0100

    feat: export activityHeatmapHelpers from utils/index

--- web/src/utils/index.ts
+++ web/src/utils/index.ts
@@ -1,4 +1,11 @@
 export { blocksToSeconds, secondsToUnits, padTwo, buildAriaLabel } from './countdownUtils';
+export {
+  heatmapSlotKey,
+  parseSlotKey,
+  slotsToHeatmapData,
+  isValidHeatmapTimestamp,
+  MAX_HEATMAP_SLOTS,
+} from './activityHeatmapHelpers';
 export { calculateStackingYield } from './calculateStackingYield';
 export {
   formatBtcAmount,
