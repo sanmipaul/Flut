49da5e8198f9e54bd73ee9e0d33f16d77001aa5d
sanmipaul <sanmipaul1@gmail.com>
2026-05-01 22:14:49 +0100

    test: cancel-ownership-transfer succeeds for current vault owner

--- tests/flut_test.ts
+++ tests/flut_test.ts
@@ -288,3 +288,19 @@ Clarinet.test({
     assertEquals(block.receipts[0].result, 'false');
   }
 });
+
+Clarinet.test({
+  name: "cancel-ownership-transfer: succeeds for current vault owner",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const owner = accounts.get('wallet_1')!;
+    const newOwner = accounts.get('wallet_2')!;
+    chain.mineBlock([
+      Tx.contractCall('flut', 'create-vault', [types.uint(1000), types.uint(200)], owner.address),
+      Tx.contractCall('flut', 'initiate-ownership-transfer', [types.uint(0), types.principal(newOwner.address)], owner.address)
+    ]);
+    const block = chain.mineBlock([
+      Tx.contractCall('flut', 'cancel-ownership-transfer', [types.uint(0)], owner.address)
+    ]);
+    assertEquals(block.receipts[0].result, '(ok true)');
+  }
+});
